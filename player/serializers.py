d7eb5a02b573c0db09a82dfb99a9e27b41218ae8
HafizHuzaifa76 <hafizm.huzaifa1234gf@gmail.com>
2026-06-06 20:58:09 +0500

    add current team in player response

--- player/serializers.py
+++ player/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 
 from player.models import Player
+from teams.basic_serializers import TeamBasicSerializer
 
 class PlayerSerializer(serializers.ModelSerializer):
+    team = TeamBasicSerializer(read_only = True)
     class Meta:
         model = Player
-        fields = ['id','name', 'age', 'email']
+        fields = ['id','name', 'age', 'email', 'team']
---
+++ teams/basic_serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from teams.models import Team
+
+class TeamBasicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = [
+            'id',
+            'name',
+            'identifier',
+            'rank'
+        ]
