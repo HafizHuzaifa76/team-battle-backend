5526bd8a56529747264623ee562e0f698d7287cd
HafizHuzaifa76 <hafizm.huzaifa1234gf@gmail.com>
2026-06-06 19:29:41 +0500

    add players in team while creating and updating team

--- teams/serializers.py
+++ teams/serializers.py
@@ -1,9 +1,36 @@
+from django.forms import ValidationError
 from rest_framework import serializers
-
+from player.models import Player
 from teams.models import Team
 
 class TeamSerializer(serializers.ModelSerializer):
+    player_ids = serializers.ListField(
+        child=serializers.IntegerField(),
+        write_only=True
+    )
 
+    def validate_player_ids(self, value):
+        print(f'validating player ids: {len(value)}')
+        
+        if len(value) == 0:
+            raise serializers.ValidationError(
+                "At least one player is required."
+            )
+        
+        team = self.instance
+
+        qs = Player.objects.filter(id__in = value, team__isnull = False)
+
+        if team:
+            qs = qs.exclude(team = team)
+        
+        if qs.exists():
+            raise serializers.ValidationError(
+                "Some players already belong to another team."
+            )
+
+        return value
+        
     class Meta:
         model = Team
 
@@ -11,7 +38,8 @@ class TeamSerializer(serializers.ModelSerializer):
             'id',
             'name',
             'identifier',
-            'rank'
+            'rank',
+            'player_ids',
         ]
 
         read_only_fields = ['rank', 'identifier']
--- teams/services.py
+++ teams/services.py
@@ -3,6 +3,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import _get_object_or_404
 from accounts.models import User
+from player.models import Player
 from teams.models import Team
 
 
@@ -13,7 +14,8 @@ def create_team(validated_data):
     if last_team:
         next_rank = last_team.rank + 1
     
-    name = validated_data['name']
+    name = validated_data.get('name')
+    player_ids = validated_data.get('player_ids')
     identifier = '_'.join(str(name).strip().lower().split())
 
     team = Team.objects.create(
@@ -22,20 +24,36 @@ def create_team(validated_data):
         rank=next_rank
     )
 
+    Player.objects.filter(id__in = player_ids, team__isnull=True).update(team=team)
+
     # User.objects.create_user(
     #     email = identifier,
     #     password = 'player123'
     # )
+
+    
 
     return team
 
 def edit_team(team_id, validated_data):
     team = get_object_or_404(Team, id=team_id)
     
+    new_player_ids = validated_data.get('player_ids')
+
     for key, value in validated_data.items():
         setattr(team, key, value)
     
     team.save()
+
+    Player.objects.filter(team=team).exclude(
+        id__in=new_player_ids
+    ).update(team=None)
+
+    Player.objects.filter(
+        id__in=new_player_ids,
+        team__isnull=True
+    ).update(team=team)
+
     return team
 
 def delete_team(team_id):
