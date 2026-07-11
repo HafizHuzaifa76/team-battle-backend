97fb207ccb965b6b97781185ea638ce97cb15b8e
HafizHuzaifa76 <hafizm.huzaifa1234gf@gmail.com>
2026-07-11 17:20:41 +0500

    replace to postrgre sql db

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,16 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
+
+# print("DATABASE_URL =", repr(os.getenv("DATABASE_URL")))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -94,10 +100,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 REST_FRAMEWORK = {
